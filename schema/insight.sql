20043ea27aaa8d162d9297122520af65a1a1477b
Abhishek Kandoi <abhikandoi2000@gmail.com>
2014-01-26 02:01:14 +0530

    Updates schema to account for unique and autoincrementing fields

--- schema/insight.sql
+++ schema/insight.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 25, 2014 at 10:53 PM
+-- Generation Time: Jan 26, 2014 at 02:00 AM
 -- Server version: 5.5.34-0ubuntu0.12.04.1
 -- PHP Version: 5.3.10-1ubuntu3.9
 
@@ -23,6 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `commits`
+--
+
+CREATE TABLE IF NOT EXISTS `commits` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `additions` int(10) NOT NULL,
+  `deletions` int(10) NOT NULL,
+  `files_affected` int(6) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `member_id` int(10) NOT NULL,
+  `project_id` int(10) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Commit Details' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `language_stats`
 --
 
@@ -31,6 +49,43 @@ CREATE TABLE IF NOT EXISTS `language_stats` (
   `language` varchar(20) NOT NULL,
   `percentage` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Language Stats';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `mail` varchar(256) NOT NULL,
+  `github_id` varchar(20) NOT NULL,
+  `birthday` date NOT NULL,
+  `year` int(1) NOT NULL,
+  `since` datetime NOT NULL,
+  `group` varchar(10) NOT NULL,
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `mail` (`mail`),
+  UNIQUE KEY `github_id` (`github_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Member details' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `homepage` varchar(256) NOT NULL,
+  `identifier` varchar(20) NOT NULL,
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `identifier` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Project Details' AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
